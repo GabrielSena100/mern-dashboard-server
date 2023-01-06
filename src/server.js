4695fa371907fac075538154de3d844dd7c26090
GabrielSena100 <gabrielsena500@gmail.com>
2023-01-06 20:41:06 -0300

    Added performance routes

--- src/server.js
+++ src/server.js
@@ -13,7 +13,8 @@ import Product from './models/Product.js'
 import ProductStat from './models/ProductStat.js'
 import Transaction from './models/Transaction.js'
 import OverallStat from './models/OverallStat.js'
-import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from './data/index.js'
+import AffiliateStat from './models/AfiliateStat.js'
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat, dataAffiliateStat } from './data/index.js'
 
 /**CONFIGURATION */
 dotenv.config();
@@ -46,6 +47,7 @@ mongoose.connect(process.env.MONGO_URL, {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /**ONLY ADD DATA ONE TIME */
+    //AffiliateStat.insertMany(dataAffiliateStat);
     //User.insertMany(dataUser);
     //Product.insertMany(dataProduct);
     //OverallStat.insertMany(dataOverallStat);
