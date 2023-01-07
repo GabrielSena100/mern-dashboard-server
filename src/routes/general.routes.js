cd6207a4b53983fb697855743c29bc77dc5dfaac
GabrielSena100 <gabrielsena500@gmail.com>
2023-01-07 10:33:25 -0300

    Added dashboard routes

--- src/routes/general.routes.js
+++ src/routes/general.routes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { getUser } from '../controllers/generalController.js'
+import { getUser, getDashboardStats } from '../controllers/generalController.js'
 
 const router = express.Router();
 
 router.get("/user/:id", getUser);
 router.get("/", (req, res) => {
     res.status(200).json({message: "works"})
-})
+});
+router.get("/dashboard", getDashboardStats);
 export default router;
